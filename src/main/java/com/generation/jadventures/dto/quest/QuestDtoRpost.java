8d895cf53cc7bda2091cc22434c1253f09990165
ciotoliveronica <veronicaciotolivc@gmail.com>
2024-02-22 15:06:32 +0100

    converter

---
+++ src/main/java/com/generation/jadventures/dto/quest/QuestDtoRpost.java
@@ -0,0 +1,17 @@
+package com.generation.jadventures.dto.quest;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@SuperBuilder
+public class QuestDtoRpost extends QuestDtoBase 
+{
+    private Integer id;
+    private Integer guild_id;
+}
