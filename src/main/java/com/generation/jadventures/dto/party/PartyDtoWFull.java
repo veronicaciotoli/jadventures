5e47ead33a85453233d6e3217ba9a819775b97d5
ciotoliveronica <veronicaciotolivc@gmail.com>
2024-02-26 11:45:16 +0100

    versioneController

---
+++ src/main/java/com/generation/jadventures/dto/party/PartyDtoWFull.java
@@ -0,0 +1,24 @@
+package com.generation.jadventures.dto.party;
+
+import java.util.List;
+
+import com.generation.jadventures.model.entities.Adventurer;
+import com.generation.jadventures.model.entities.Quest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class PartyDtoWFull 
+{
+    private Integer id;
+    private String name,authentication_seal,motto;
+    private Adventurer party_leader;
+    private List<Quest> quests;
+    private List<Adventurer> adventurers;
+}
