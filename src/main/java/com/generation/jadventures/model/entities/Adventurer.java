a90664b04274064725eae0fe459a82ab725ddbeb
ciotoliveronica <veronicaciotolivc@gmail.com>
2024-02-26 09:21:34 +0100

    versione1

---
+++ src/main/java/com/generation/jadventures/model/entities/Adventurer.java
@@ -0,0 +1,41 @@
+package com.generation.jadventures.model.entities;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Adventurer 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; 
+    private String name, surname; 
+    private LocalDate dob; 
+    private String rank;
+    private String role; 
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "party_id")
+    private Party party; 
+
+
+}
